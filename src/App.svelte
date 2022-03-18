46c6ea846927d5bdaaba0a2bd2d26f0c6d677121
m93a <m93a.cz@gmail.com>
2022-03-18 16:33:42 +0100

    scratch blocks do something now haha

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+  import ScratchBlocks from './lib/ScratchBlocks.svelte'
 </script>
 
 <main>
   <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <h1>Hello Scratch Blocks!</h1>
+  <ScratchBlocks />
 </main>
 
 <style>
---
+++ src/lib/ScratchBlocks.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+    declare const Blockly: any;
+</script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+    import '../../../scratch-blocks/dist/web/vertical';
+
+    onMount(() => {
+        Blockly.inject("scratch-blocks", {
+			horizontalLayout: false,
+			media: '../../../scratch-blocks/media/',
+		});
+    });
+</script>
+
+<div id="scratch-blocks">
+    
+</div>
+
+<style lang="scss">
+    #scratch-blocks {
+        width: 90vw;
+        height: 50vh;
+    }
+</style>
+
+
